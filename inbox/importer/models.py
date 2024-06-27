397d8b7b2d89e6bb361919d0a2c3140a164e4601
Albert Safin <albertsafinf@gmail.com>
2024-06-27 14:18:27 +0500

    Create importer app models. Model Account stores credentials of email accounts. Model Message stores email message data and attached files.

--- inbox/importer/models.py
+++ inbox/importer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+
+
+class Message(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    date_sent = models.DateTimeField()
+    date_received = models.DateTimeField()
+    body = models.TextField()
+    attachments = models.FileField(
+        upload_to="attachments/%Y/%m/%d/",
+        blank=True,
+        null=True,
+    )
