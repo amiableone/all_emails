b0c0c8815fb6edf8a6f55a7abc9eaacc637f64bd
Albert Safin <albertsafinf@gmail.com>
2024-06-29 14:24:04 +0500

    Create google_oauth2_cb() util func. Create credentials_to_dict() util func.

--- inbox/importer/utils.py
+++ inbox/importer/utils.py
@@ -17,3 +17,27 @@ def google_oauth2(login, redirect_uri):
         prompt="consent",
     )
     return auth_url, state
+
+
+def credentials_to_dict(credentials):
+    return {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes,
+    }
+
+
+def google_oauth2_cb(state, redirect_uri, auth_resp):
+    # State is retrieved from session in CompleteGoogleAuthView.get().
+    flow = Flow.from_client_secrets_file(
+        CLIENT_SECRET,
+        scopes=SCOPES,
+        state=state,
+    )
+    flow.redirect_uri = redirect_uri
+    flow.fetch_token(authorization_response=auth_resp)
+    credentials = flow.credentials
+    return credentials_to_dict(credentials)
