2517ea2292fef88e3f5f8c9ead16fcfbaaf087ee
Albert Safin <albertsafinf@gmail.com>
2024-06-29 19:44:07 +0500

    Update the Account model. Add field user to control uniqueness of emails as per user basis. Add unique constraint on the user-email column pair.

--- inbox/importer/models.py
+++ inbox/importer/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 
 class Account(models.Model):
-    email = models.CharField(max_length=255, unique=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    email = models.EmailField()
     credentials = models.JSONField()
+
+    class Meta:
+        constraints = [
+            # Each email can only be added once by each user.
+            models.UniqueConstraint(
+fields=["user", "email"],
+                name="unique_emails_per_user",
+            )
+        ]
 
 
 class Message(models.Model):
