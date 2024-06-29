a0ec2e6c986e5e2208232f500018e2e8fb49f75b
Albert Safin <albertsafinf@gmail.com>
2024-06-29 13:45:11 +0500

    Update google_oauth2() func Assign path to client_secret.json and scopes list to module level vars. Comment on the func op flow. Return a tuple of auth_url and state (remove flow).

--- inbox/importer/utils.py
+++ inbox/importer/utils.py
@@ -1,15 +1,19 @@
 from google_auth_oauthlib.flow import Flow
 
+CLIENT_SECRET = "../../client_secret.json"
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
 
 def google_oauth2(login, redirect_uri):
-    flow = Flow.from_client_secrets_file(
-        "../../client_secret.json",
-        scopes=["https://www.googleapis.com/auth/gmail.readonly"],
-    )
+    flow = Flow.from_client_secrets_file(CLIENT_SECRET, scopes=SCOPES)
+    # Pass redirect_uri to the current func from GoogleAuthView.
     flow.redirect_uri = redirect_uri
+    # Generate url of Google's OAuth2.0 server to redirect a user to.
+    # The server will authenticate and obtain consent from the user to
+    # read messages from their Gmail inbox.
     auth_url, state = flow.authorization_url(
         access_type="offline",
         login_hint=login,
         prompt="consent",
     )
-    return flow, auth_url, state
+    return auth_url, state
