16ee327f9a3033e7e8d2bb6125c80c37639ea478
Albert Safin <albertsafinf@gmail.com>
2024-06-29 13:50:08 +0500

    Create GoogleAuthView In get(), generate authentication url to authenticate Gmail user and obtain consent from them to read their emails. Save state in the session. Redirect to the authentication url.

--- inbox/importer/views.py
+++ inbox/importer/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+from .utils import google_oauth2
+
+
+class GoogleAuthView(View):
+    def get(self, request, *args, **kwargs):
+        login = request.user.email
+        # Tell Google's OAuth2.0 server to redirect to redirect_uri
+        # on user consent being granted.
+        redirect_uri = reverse("importer:complete-google-oauth")
+        auth_url, state = google_oauth2(login, redirect_uri)
+        # Pass state as session parameter to protect from CSRF by passing
+        # the parameter value from CompleteGoogleAuthView.get(), which will
+        # handle the redirect back from the Google's OAuth2.0 server, to
+        # Flow object constructor.
+        request.session["state"] = state
+        request.session.modified = True
+        return HttpResponseRedirect(auth_url)
