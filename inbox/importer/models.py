3355f3baf8670a84c666ef80816209f523b32163
Albert Safin <albertsafinf@gmail.com>
2024-06-29 18:42:50 +0500

    Update model Account. Replace the password field with credentials JSONField.

--- inbox/importer/models.py
+++ inbox/importer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Account(models.Model):
     email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    credentials = models.JSONField()
 
 
 class Message(models.Model):
